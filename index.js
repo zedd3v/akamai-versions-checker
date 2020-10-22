015686d808ae3df0104edf2a8f8bcdd8ad2a4f43
ZedDev <43217974+zedd3v@users.noreply.github.com>
2020-10-22 22:46:39 +0200

    initial

---
+++ index.js
@@ -0,0 +1,74 @@
+// @ts-nocheck
+
+const got = require('got').default;
+
+const WEBSITES = [
+  'https://www.xfinity.com',
+  'https://www.ups.com',
+  'https://www.fedex.com',
+  'https://www.banggood.com',
+  'https://www.homedepot.com',
+  'https://www.naukri.com',
+  'https://www.fidelity.com',
+  'https://www.groupon.com',
+  'https://www.blibli.com',
+  'https://www.att.com',
+  'https://www.lenovo.com',
+  'https://www.discover.com',
+  'https://www.mobile.de',
+  'https://www.zara.com',
+  'https://www.olx.in',
+  'https://www.creditkarma.com',
+  'https://www.lowes.com',
+  'https://www.usbank.com',
+  'https://www.marriott.com',
+  'https://www.farfetch.com',
+  'https://www.dhl.de',
+  'https://www.myntra.com',
+  'https://www.g2a.com',
+  'https://www.concursolutions.com',
+];
+
+(async () => {
+  WEBSITES.forEach(async (url) => {
+    const { body } = await got(url, {
+      https: {
+        rejectUnauthorized: false,
+      },
+      headers: {
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+        Pragma: 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+      },
+    });
+
+    const scriptUrl = /\['_setAu', '(\/\w+\/\w+)'\]/i.exec(body);
+
+    const res = await got(`${url}${scriptUrl[1]}`, {
+      https: {
+        rejectUnauthorized: false,
+      },
+      headers: {
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+        Pragma: 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+      },
+    });
+
+    console.log(`${url}${scriptUrl[1]}`, res.body.split('ver:')[1].split(',ke_cnt_lmt')[0]);
+  });
+})();
